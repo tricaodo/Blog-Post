bd7536f1ea194103a7aed1c3e87c250c0929503d
Tri Do <trido1903@gmail.com>
2020-05-28 12:05:11 -0700

    working on project details

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
+import Dashboard from "./components/dashboard/Dashboard";
+import ProjectDetails from "./components/projects/ProjectDetails";
 
 function App() {
   return (
     <BrowserRouter>
       <div>
         <Navbar />
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+          <Route path="/project/:id" component={ProjectDetails} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
---
+++ src/components/dashboard/Notification.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Notification extends Component {
+  render() {
+    return (
+      <div>
+        <p>Notification</p>
+      </div>
+    );
+  }
+}
+
+export default Notification;
