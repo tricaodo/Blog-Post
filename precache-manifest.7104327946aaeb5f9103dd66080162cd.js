05f183b318706af1480a53bbe6f19426b72cfd56
Tri Do <trido1903@gmail.com>
2020-07-25 13:21:08 -0700

    Updates

---
+++ precache-manifest.7104327946aaeb5f9103dd66080162cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b53e5aafc3145ee2b921d62698682efd",
+    "url": "/tricaodo/TrisBlog/index.html"
+  },
+  {
+    "revision": "9679d7daed69bcf3dea2",
+    "url": "/tricaodo/TrisBlog/static/css/main.e01e262d.chunk.css"
+  },
+  {
+    "revision": "153e9fe30324f21a8115",
+    "url": "/tricaodo/TrisBlog/static/js/2.421af236.chunk.js"
+  },
+  {
+    "revision": "ba9f1cbbd5250a36f31fb5b559f6f707",
+    "url": "/tricaodo/TrisBlog/static/js/2.421af236.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9679d7daed69bcf3dea2",
+    "url": "/tricaodo/TrisBlog/static/js/main.696661f5.chunk.js"
+  },
+  {
+    "revision": "3c2fb04dead2b1c15a5a",
+    "url": "/tricaodo/TrisBlog/static/js/runtime-main.1acef864.js"
+  }
+]);
